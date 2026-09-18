d08a1d56f1c43d5e6e84aaa42d814e08553d9a85
Beshoy-Edwar-Aziz <137794249+Beshoy-Edwar-Aziz@users.noreply.github.com>
2026-09-18 18:03:43 +0300

    Refactor birthday email logic and data handling
    Refactored birthday email sending logic to use a dictionary for birthday data and simplified the process of checking for today's birthdays.

--- main.py
+++ main.py
@@ -16,23 +16,28 @@ import os
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# 1. Update the birthdays.csv
+birthday_date = pandas.read_csv("birthdays.csv")
+birthday_dict = birthday_date.to_dict(orient="records")
+# 2. Check if today matches a birthday in the birthdays.csv
+now = dt.datetime.now()
+birth_day_name = ""
+birth_day_email = ""
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+for birth in birthday_dict:
+    if birth.get("month") == now.month and birth.get("day") == now.day:
+        birth_day_name = birth.get("name")
+        birth_day_email = birth.get("email")
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+        with open(f"letter_templates/letter_{random.randint(1,3)}.txt") as letter1:
+            content = letter1.read()
+            new_text = content.replace("[NAME]",birth_day_name)
+# 4. Send the letter generated in step 3 to that person's email address.
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL,
+                                to_addrs=birth_day_email,
+                                msg="Subject:Happy Birthday!\n\n"
+                                    f"{new_text}")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
